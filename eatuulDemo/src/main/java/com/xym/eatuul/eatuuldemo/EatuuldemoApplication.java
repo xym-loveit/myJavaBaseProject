b5361b071a3f5339f77bf130ce1c9f5a12f90b4b
xiangtao <xym_loveit@126.com>
2018-09-05 18:09:56 +0800

    add some file

---
+++ eatuulDemo/src/main/java/com/xym/eatuul/eatuuldemo/EatuuldemoApplication.java
@@ -0,0 +1,12 @@
+package com.xym.eatuul.eatuuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatuuldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatuuldemoApplication.class, args);
+    }
+}
---
+++ eatuulDemo/src/test/java/com/xym/eatuul/eatuuldemo/EatuuldemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xym.eatuul.eatuuldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EatuuldemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
