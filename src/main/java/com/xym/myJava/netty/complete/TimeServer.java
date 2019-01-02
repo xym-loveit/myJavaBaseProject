4e48273a2d4273176f1c0c2af9d022cb959e8057
xiangtao <xym_loveit@126.com>
2019-01-02 15:17:17 +0800

    add netty

---
+++ src/main/java/com/xym/myJava/netty/complete/TimeServer.java
@@ -0,0 +1,109 @@
+package com.xym.myJava.netty.complete;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * netty实现的nio服务端
+ *
+ * @author xym
+ * @create 2019-01-02 9:54
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                port = 8080;
+            }
+        }
+        new TimeServer().bind(port);
+    }
+
+    public void bind(int port) {
+        //配置服务端NIO线程组,一个用于接受客户端连接，一个用于网络读写
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            //netty nio服务端的辅助启动类，目的是降低服务端的开发复杂度
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChildChannelHandler());
+            //ChildChannelHandler为io事件处理类
+            //绑定端口，同步等待成功
+            ChannelFuture f = bootstrap.bind(port).sync();
+            //等待服务器端监听端口关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //优雅退出释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public class TimeServerHandler extends ChannelHandlerAdapter {
+
+        private int counter;
+
+        /**
+         * 当发生异常时，关闭ChannelHandlerContext，释放和ChannelHandlerContext相关联的句柄等资源
+         *
+         * @param ctx
+         * @param cause
+         * @throws Exception
+         */
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            String body = (String) msg;
+            //readableBytes可以获取到缓冲区可读字节数
+            //byte[] bytes = new byte[buffer.readableBytes()];
+            //buffer.readBytes(bytes);
+            //String body = new String(bytes, "UTF-8").substring(0, bytes.length - System.getProperty("line.separator").length());
+            System.out.println("the time server receiver order :" + body + ",the counter is :" + (++counter));
+            String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+            currentTime = currentTime + System.getProperty("line.separator");
+            ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+            ctx.writeAndFlush(resp);
+        }
+
+        /**
+         * 将消息发送队列中的消息写入到SocketChannel中发送给对方，从性能角度考虑，为了防止频繁
+         * 唤醒Selector进行消息发送，Netty的write方法并不直接将消息写入到SocketChannel中，调用write
+         * 方法只是把待发送的消息放到发送缓冲数组中，再通过调用flush方法，将发送缓冲区中的消息全部写到SocketChannel中
+         *
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+        }
+    }
+}
---
+++ src/main/java/com/xym/myJava/netty/incomplete/TimeClient.java
@@ -0,0 +1,100 @@
+package com.xym.myJava.netty.incomplete;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * netty实现的nio客户端(观察结果发现发生了粘包问题)
+ *
+ * @author xym
+ * @create 2019-01-02 11:14
+ */
+public class TimeClient {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                port = 8080;
+            }
+        }
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
+    public void connect(int port, String host) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).
+                    option(ChannelOption.TCP_NODELAY, true).handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new TimeClientHandler());
+                }
+            });
+            //发起异步链接操作
+            ChannelFuture f = bootstrap.connect(host, port).sync();
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //优雅退出，释放nio线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    private class TimeClientHandler extends ChannelHandlerAdapter {
+
+        private int counter;
+        private byte[] req;
+
+        public TimeClientHandler() {
+            req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+
+        /**
+         * 当服务端返回消息时，该方法被调用
+         *
+         * @param ctx
+         * @param msg
+         * @throws Exception
+         */
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.readBytes(bytes);
+            String body = new String(bytes, "UTF-8");
+            System.out.println("Now is :" + body + ",the counter is :" + (++counter));
+        }
+
+        /**
+         * 当客户端和服务器TCP链路建立成功后，netty nio线程会调用该方法
+         *
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ByteBuf message = null;
+            for (int i = 0; i < 100; i++) {
+                message = Unpooled.buffer(req.length);
+                message.writeBytes(req);
+                ctx.writeAndFlush(message);
+            }
+        }
+    }
+}
